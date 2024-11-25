dd1cae869d65693196020a30ce32476bf8f1569e
Sergio Valadão Junior <savaladaojr@gmail.com>
2024-11-25 22:07:30 +0000

    alterando a frase.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return <div>
-                <h1>Pagina Inicial.</h1>
-                <h2>Hoje é um novo dia.</h2>
+                <h1>"Todos os seus sonhos podem se tornar realidade se você tiver a coragem de persegui-los."</h1>
+                <h2><i>Walt Disney</i></h2>
             </div>
 }
 
